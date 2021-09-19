240c95ed164685267db374739c257a3c6326c3aa
radamr <rybicki-adam@wp.pl>
2021-09-19 19:19:21 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,78 @@
+let number = 0
+input.onGesture(Gesture.Shake, function () {
+    number = randint(1, 6)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    if (number == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once)
+    } else if (number == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (number == 3) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (number == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (number == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (number == 6) {
+        basic.showLeds(`
+            # . # . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . # . #
+            `)
+        music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+    } else {
+        basic.showIcon(IconNames.Sad)
+    }
+})
